b5417ba53dc6ee669056d70c1db25acf8e9a13b8
lebowski505 <dmitriysalienko@gmail.com>
2024-12-06 00:51:44 +0200

    added product top

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 $(function(){
 
+  
+const filterButtons = document.querySelectorAll('.product-top__filter-btn');
+const contentItems = document.querySelectorAll('.product-top__content-item');
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const filter = button.getAttribute('data-filter'); 
+
+    contentItems.forEach(item => {
+      if (filter === 'all' || item.classList.contains(filter.replace('.', ''))) {
+        item.classList.remove('hidden'); 
+      } else {
+        item.classList.add('hidden'); 
+      }
+    });
+
+    filterButtons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+  
   $('.top-slider__items').slick({
     dots: true,
     arrows: false,
