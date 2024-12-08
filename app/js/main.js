e9731f0bc1fdeda0f5bf719737a14f85e147bd59
lebowski505 <dmitriysalienko@gmail.com>
2024-12-08 17:59:33 +0200

    added articles

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,41 @@
-$(function(){
+$(function () {
+  
 
   
-const filterButtons = document.querySelectorAll('.product-top__filter-btn');
-const contentItems = document.querySelectorAll('.product-top__content-item');
 
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const filter = button.getAttribute('data-filter'); 
 
-    contentItems.forEach(item => {
-      if (filter === 'all' || item.classList.contains(filter.replace('.', ''))) {
-        item.classList.remove('hidden'); 
-      } else {
-        item.classList.add('hidden'); 
-      }
-    });
-
-    filterButtons.forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
-  });
-});
-
-  
   $('.top-slider__items').slick({
     dots: true,
     arrows: false,
     fade: true,
   });
+  
+  
+  
+  const setupFilterFunctionality = (filterSelector, contentSelector, activeClass = 'active', hiddenClass = 'hidden') => {
+    const filterButtons = document.querySelectorAll(filterSelector);
+    const contentItems = document.querySelectorAll(contentSelector);
 
+    filterButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const filter = button.getAttribute('data-filter');
+
+        contentItems.forEach(item => {
+          if (filter === 'all' || item.classList.contains(filter.replace('.', ''))) {
+            item.classList.remove(hiddenClass);
+          } else {
+            item.classList.add(hiddenClass);
+          }
+        });
+
+        filterButtons.forEach(btn => btn.classList.remove(activeClass));
+        button.classList.add(activeClass);
+      });
+    });
+  };
+
+  setupFilterFunctionality('.product-top__filter-btn', '.product-top__content-item');
+  setupFilterFunctionality('.design__filter-btn', '.design__content-item');
+
+  
 });
