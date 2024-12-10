7a535a9eea6a677259ea4a3f0e1ee5188aa82716
lebowski505 <dmitriysalienko@gmail.com>
2024-12-10 15:31:11 +0200

    added all

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,45 @@
+
 $(function () {
   
-
-  
-
 
   $('.top-slider__items').slick({
     dots: true,
     arrows: false,
     fade: true,
   });
+
+
+  $(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+      type: 'iframe',
+      iframe: {
+        patterns: {
+          youtube: {
+            index: 'youtube.com/',
+            id: 'v=',
+            src: 'https://www.youtube.com/embed/%id%?autohide=1'
+          }
+        }
+      }
+    });
   
+    // Перевірка чи працює Magnific Popup
+    $('.popup-youtube').on('click', function(event) {
+      event.preventDefault(); 
+      event.stopPropagation(); 
+      $(this).magnificPopup('open');
+    });
+  });
+
   
-  
+  $('.popup-youtube').on('click', function(event) {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+
+    
+    $(this).magnificPopup('open');
+  });
+
   const setupFilterFunctionality = (filterSelector, contentSelector, activeClass = 'active', hiddenClass = 'hidden') => {
     const filterButtons = document.querySelectorAll(filterSelector);
     const contentItems = document.querySelectorAll(contentSelector);
@@ -36,6 +64,4 @@ $(function () {
 
   setupFilterFunctionality('.product-top__filter-btn', '.product-top__content-item');
   setupFilterFunctionality('.design__filter-btn', '.design__content-item');
-
-  
 });
